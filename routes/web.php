eedcb18a58220382ffa58639daacb0f3172b4b72
Philipp Mark Desquitado <0720746@cc.usls.edu>
2026-01-22 10:25:18 +0800

    Lab Activity 1: Routes, Middleware, Layouts

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,15 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('home');
 });
+
+Route::get('/about', function () {
+    return view('about');
+});
+
+Route::get('/projects', function () {
+    return view('projects');
+});
+
+Route::get('/contacts', function () {
+    return view('contacts');
+});
