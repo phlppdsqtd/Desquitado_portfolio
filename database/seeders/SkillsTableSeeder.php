fa999221ccfcfefe37c256107378f99160922b68
Philipp Mark Desquitado <0720746@cc.usls.edu>
2026-01-29 10:10:42 +0800

    Lab Activity 2 v2.0: Migration and Seeder

--- database/seeders/SkillsTableSeeder.php
+++ database/seeders/SkillsTableSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class SkillsTableSeeder extends Seeder
 {
@@ -12,6 +13,12 @@ class SkillsTableSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        DB::table('skills')->insert([
+            ['name' => 'HTML', 'level' => 'Advanced'],
+            ['name' => 'CSS', 'level' => 'Intermediate'],
+            ['name' => 'JavaScript', 'level' => 'Intermediate'],
+            ['name' => 'PHP', 'level' => 'Advanced'],
+            ['name' => 'Laravel', 'level' => 'Beginner'],
+        ]);
     }
 }
